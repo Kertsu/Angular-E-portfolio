948bb587606096b14eb71205d806b4c83d0651a9
Kertsu <kurtddbigtas@gmail.com>
2023-08-18 00:40:51 +0800

    updated routing bug

--- src/app/components/mobile-nav/mobile-nav.component.ts
+++ src/app/components/mobile-nav/mobile-nav.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, Input, Renderer2 } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+import { UiService } from 'src/app/services/ui.service';
 @Component({
   selector: 'app-mobile-nav',
   templateUrl: './mobile-nav.component.html',
@@ -7,9 +9,15 @@ import { Router } from '@angular/router';
 })
 export class MobileNavComponent {
   routes: any;
+  isClosing: boolean = false;
   @Input() showMenu!: boolean;
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private uiService: UiService,
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document
+  ) {
     this.routes = [
       { routerLink: '/', title: 'Home' },
       { routerLink: '/education', title: 'Education' },
@@ -17,6 +25,19 @@ export class MobileNavComponent {
       { routerLink: '/friends', title: 'Friends' },
       { routerLink: '/interests', title: 'Interests' },
     ];
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        console.log('start', event);
+        this.isClosing = true;
+      } else if (event instanceof NavigationEnd) {
+        console.log('end', NavigationEnd);
+        // Delay the reset of isClosing to allow animation to complete
+        setTimeout(() => {
+          this.isClosing = false;
+        }, 300);
+      }
+    });
   }
   isActive(routeLink: string): boolean {
     // Get the current URL
@@ -24,5 +45,16 @@ export class MobileNavComponent {
 
     // Check if the current URL matches the provided routeLink
     return currentUrl === routeLink;
+  }
+
+  clickedA() {
+    this.uiService.toggleMenu();
+    this.renderer.setStyle(this.document.body, 'overflow-y', 'visible');
+  }
+  clickedLi(event: Event) {
+    this.uiService.toggleMenu();
+    this.renderer.setStyle(this.document.body, 'overflow-y', 'visible');
+
+    event.stopPropagation();
   }
 }
