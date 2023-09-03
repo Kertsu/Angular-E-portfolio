09e48e4b756595113ad915e4b12014984c86b256
Kertsu <kurtddbigtas@gmail.com>
2023-09-04 07:31:21 +0800

    added sidebar animation on route change

--- src/app/components/layout/layout/layout.component.ts
+++ src/app/components/layout/layout/layout.component.ts
@@ -1,23 +1,42 @@
-import { Component, ElementRef, Inject, Renderer2 } from '@angular/core';
+import { Component, ElementRef, Inject, Renderer2, OnInit } from '@angular/core';
 import { Base } from '../../Base';
 import { DOCUMENT } from '@angular/common';
 import { StyleService } from 'src/app/services/style.service';
 import { UiService } from 'src/app/services/ui.service';
+import { NavigationEnd, Router } from '@angular/router';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout'
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent extends Base {
+export class LayoutComponent extends Base implements OnInit{
   constructor(
     protected override uiService: UiService,
     protected override renderer: Renderer2,
     protected override styleService: StyleService,
     protected override elementRef: ElementRef,
-    @Inject(DOCUMENT) protected override document: Document
+    @Inject(DOCUMENT) protected override document: Document,
+    private router: Router,
+    private breakpointObserver: BreakpointObserver
   ){
     super(uiService, renderer, styleService, elementRef, document);
 
   }
+  ngOnInit(): void {
+    
+
+    this.breakpointObserver.observe('(max-width:991px)').subscribe((state)=>{
+      if (state.matches){
+      this.router.events.subscribe((event)=>{
+      if (event instanceof NavigationEnd){
+            this.setToggle()
+          }
+        })
+      }
+    })
+    
+  }
+  
 }
